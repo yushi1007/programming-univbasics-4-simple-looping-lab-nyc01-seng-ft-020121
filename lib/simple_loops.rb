00dcddccbcfab3d2687b26dd9c7e765305fd80e9
Yu Shi <yushiys95@gmail.com>
2021-01-16 03:26:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,10 @@ def loop_message_n_times(message, limit)
     count += 1
   end
 end
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
